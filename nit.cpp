04a4a9692807a997a27dcca9efe528c64d0f69df
Nitpreet <32680124+Nitpreet@users.noreply.github.com>
2017-10-10 16:40:50 +0000

    Add files via upload

---
+++ nit.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+ {
+	int a,b;
+     cout << "Enter two numbers" << endl;
+	cin>>a>>b;
+	cout<<"The sum is "<<a+b<<endl;
+	
+     return(0);
+
+	}
+
